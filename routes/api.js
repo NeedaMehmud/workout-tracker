8acaf2a99ed722f619e895d6a78bf64ab6b363c5
Needa Mehmud <needa.mehmud@gmail.com>
2021-08-14 21:13:47 -0400

    get all workouts api route complete

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,29 @@
-const db = require("../models/Workout");
+const db = require('../models/Workout');
 const router = require("express").Router();
 
-// Get last workout
-router.get("/api/workouts", (req, res) => {
-    console.log("inside Get last workout")
-});
+
+
+// Get all workouts
+router.get("/api/workouts/", (req, res) => {
+    db.aggregate(
+      [
+        {
+          $addFields: {
+            totalDuration: {
+              $sum: "$exercises.duration"
+            }
+          }
+        }
+      ]
+    )
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
+  
 
 
 
