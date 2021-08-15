b07d0dca3aa269ec2c504e329277a8071d242d79
Needa Mehmud <needa.mehmud@gmail.com>
2021-08-14 20:30:33 -0400

    start over to try and fix errors

---
+++ server.js
@@ -0,0 +1,27 @@
+
+const express = require('express');
+const logger = require("morgan");
+const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workout',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }
+);
+app.use(logger('dev'));
+
+app.listen(PORT, () => {
+    console.log(`App running successfully on port ${PORT}!`);
+});
